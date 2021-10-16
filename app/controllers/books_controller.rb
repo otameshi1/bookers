2bbb06897f56f16e7156c742ceaef193c4439912
RY2291 <prgr2498@gmail.com>
2021-10-16 01:31:45 +0000

    homes2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,30 @@
 class BooksController < ApplicationController
+    
+    def create
+        
+    end
+    
     def index
+        @books = Book.all
+        @book = Book.new
     end
     
     def edit
+        book = Book.find(book_params)
+    end
+    
+    def update
     end
     
     def show
+    end
+    
+    def destory
+    end
+    
+    
+    private
+    def book_params
+        require(:book).permit(:title, :body)
     end
 end
