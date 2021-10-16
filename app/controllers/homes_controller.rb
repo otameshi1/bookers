e5d67a0da774175c2a0648b56727af5373397d4a
EC2 Default User <hms0175km@gmail.com>
2021-10-16 00:14:06 +0000

    submain

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,11 @@
 class HomesController < ApplicationController
+<<<<<<< HEAD
 def top
 
 end
+=======
+
+  
+
+>>>>>>> 9423473e9db67ddfdc9cbfa8307c91a29008aaa3
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
     root to: "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+
+>>>>>>> 9423473e9db67ddfdc9cbfa8307c91a29008aaa3
 end
